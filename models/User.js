6837761c40d0206ca5c6052e901a8fc2575b3e38
PravTon <clintonsf@outlook.com>
2021-07-06 18:54:00 -0400

    added authentication and hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  //  set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -45,16 +51,28 @@ User.init(
     }
   },
   {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-    // pass in our imported sequelize connection (the direct connection to our database)
+    hooks: {
+      //  set up before Create lifecycle "hook" functionality
+      // beforeCreate(userData) {
+      //   return bcrypt.hash(userData.password, 10).then(newUserData => {
+      //     return newUserData
+      //   });
+      // }
+      //  set up before Create lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      //  set up before Create lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
-    // don't pluralize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
-    // make it so our model name stays lowercase in the database
     modelName: 'user'
   }
 );
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -48,12 +48,36 @@ router.post('/', (req, res) => {
 
 });
 
+router.post('/login', (req, res) => {
+  // expects {email: 'supermario@gmail.com', password: 'password1234' }
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if(!dbUserData) {
+      res.status(400).json({message: 'No iser with that email address!'})
+      return;
+    }
+    
+    //verify  user 
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if(!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
   // expects {username: 'supermario', email: 'supermario@gmail.com', password: 'password1234' }
 
   // If req.body has exact key/value pairs to match the model, you can use the req.body instead 
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
