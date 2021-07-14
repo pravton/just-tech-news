c2626b1ffa93864922241e4e6463ed5204589a56
PravTon <clintonsf@outlook.com>
2021-07-13 21:29:58 -0400

    added authentication features

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,7 +60,15 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-  .then(dbUserData => res.json(dbUserData))
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedin = true;
+
+      res.json(dbUserData);
+    })
+  })
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
@@ -76,7 +84,7 @@ router.post('/login', (req, res) => {
     }
   }).then(dbUserData => {
     if(!dbUserData) {
-      res.status(400).json({message: 'No iser with that email address!'})
+      res.status(400).json({message: 'No user with that email address!'})
       return;
     }
     
@@ -87,8 +95,26 @@ router.post('/login', (req, res) => {
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
+});
+
+// Logout route
+router.post('/logout', (req, res) => {
+  if(req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(400).end();
+  }
 });
 
 // PUT /api/users/1
